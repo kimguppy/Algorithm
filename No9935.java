bc27a37c36ac4c3c670ba54dd6bb104e93dd9d87
kimguppy <wonny2333@naver.com>
2023-07-07 22:16:28 +0900

    final week1

--- No9935.java
+++ No9935.java
@@ -1,21 +1,39 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class No9935 {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-        String str= scan.nextLine();
-        String str2= scan.nextLine();
-        String strNew;
-		      
-        while (true) {
-    		strNew = str.replace(str2, "");
-    		if(strNew != str) { 
-    			str=strNew;
-    		}else {        
-    			break;
-    		}
-		}
-		if(strNew == "") {System.out.println("FRULA");
-		}else{System.out.println(strNew);};
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String str = br.readLine();
+        String bomb = br.readLine();
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            stack.push(str.charAt(i));
+
+            if (stack.size() >= bomb.length()) {
+                boolean isSame = true;
+                for (int j = 0; j < bomb.length(); j++) {
+                    if (stack.get(stack.size() - bomb.length() + j) != bomb.charAt(j)) {
+                        isSame = false;
+                        break;
+                    }
+                }
+
+                if (isSame) {
+                    for (int j = 0; j < bomb.length(); j++)
+                        stack.pop();
+                }
+            }
+        }
+
+        for (char ch : stack)
+            sb.append(ch);
+
+        System.out.println(sb.length() > 0 ? sb.toString() : "FRULA");
+    }
 }
